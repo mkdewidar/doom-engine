70f9899b029062766231eb38f9815e12e5cd163b
Mohamed Dewidar <mkdewidar@gmail.com>
2018-09-07 23:27:42 +0100

    WAD: Add function to read signed 16 bit ints

---
+++ wad_reader.cpp
@@ -0,0 +1,28 @@
+// Code for reading WAD files. WAD files are the data file format used in DOOM.
+// They store all of the data related to the game (levels, enemy placement, etc)
+// Functions assume data is stored in big endian.
+
+#include "utilities.cpp"
+
+// Reads and returns a copy of a signed 2 byte integer
+// from the given data starting at the given index.
+int16_t ReadShort(char* dataArray, size_t startIndex) {
+    // upper and lower byte of the end result
+    uint8_t lowerByte;
+    uint8_t upperByte;
+
+    // depending on endianness, either the first byte in the data is the
+    // lower half or the upper half of the 2 bytes
+    if (IsSystemBigEndian()) {
+        upperByte = dataArray[startIndex];
+        lowerByte = dataArray[startIndex + 1];
+    } else {
+        lowerByte = dataArray[startIndex];
+        upperByte = dataArray[startIndex + 1];
+    }
+
+    // copying the upper half into a cleared out int
+    // then shifting it to leave the lower half empty for the lower byte
+    return ((0 | upperByte) << 8) | lowerByte;
+}
+
